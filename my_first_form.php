7c2dfa91e75d651246dad265f071afde689d6773
Adam Vega <adam.j.vega@hotmail.com>
2014-08-25 13:43:25 -0500

    initial commit

--- my_first_form.php
+++ my_first_form.php
@@ -11,19 +11,91 @@ var_dump($_GET);
 </head>
 <body>
 
-	<h2> My First HTML Form </h2>
+	<h2> User Login </h2>
 
-	<form method="POST">
+	<form method="GET">
 		<p>
-			<label for="first_name">First Name</label>
-			<input type="text" name="first_name" id="first_name"/>
+			<label for="username">Username</label>
+			<input type="text" name="username" id="username"/>
 		</p>
 		<p>
-			<label for="last_name">Last Name</label>
-			<input type="text" name="last_name" id="last_name">
+			<label for="password">Password</label>
+			<input type="password" id="password" name="password" placeholder="Enter Password" />
 		</p>
+		<p>
 			<input type="submit">
 	</form>
+	<h2> Compose and Email </h2>
+
+
+	<form method="GET">
+		<p>
+			<input type="checkbox" id="copy" name="copy" value="yes" checked>
+			<label for="copy">Would you like to have a copy sent to your folder?</label>
+		</p>
+		<p>
+			<label for="to">To</label>
+			<input type="text" name="to" id="to"/>
+		</p>
+		<p>
+			<label for="from">From</label>
+			<input type="text" name="from" id="from"/>
+		</p>
+		<p>
+			<label for="subject">Subject</label>
+			<input type="text" name="subject" id="subject"/>
+		</p>
+		<p>
+			<textarea id="email_body" name="email_body">Content Here</textarea>
+
+		</p>
+			<input type="submit" value="Send" />
+	</form>
+	<h2>Multiple Choice Test</h2>
+		<form method="GET">
+		<p>Where was Adam born?</p>
+		<label>
+			<input type="radio" id="q1a" name="q1" value="New Orleans" />
+			New Orleans
+		</label>
+				<label>
+			<input type="radio" id="q1b" name="q1" value="San Antonio" />
+			San Antonio
+		</label>
+				<label>
+			<input type="radio" id="q1c" name="q1" value="Philadelphia" />
+			Philadelphia
+		</label>
+				<label>
+			<input type="radio" id="q1d" name="q1" value="San Francisco" />
+			San Francisco
+		</label>
+				<label>
+			<input type="radio" id="q1e" name="q1" value="Corpus Christi" />
+			Corpus Christi
+		</label>
+		<p>What country does Adam's surname come from?</p>
+		<label>
+			<input type="radio" id="q2a" name="q2" value="England" />
+			England
+		</label>		
+		<label>
+			<input type="radio" id="q2b" name="q2" value="Germany" />
+			Germany
+		</label>
+		<label>
+			<input type="radio" id="q2c" name="q2" value="Ireland" />
+			Ireland
+		</label>	
+		<label>
+			<input type="radio" id="q2d" name="q2" value="Spain" />
+			Spain
+		</label>
+		<label>
+			<input type="radio" id="q2e" name="q2" value="Austria" />
+			Austria
+		</label>		
+
 </body>
 <html>
 
