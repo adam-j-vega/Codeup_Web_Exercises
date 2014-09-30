2829326160ba842be5db5d1ace8d2f2993490162
Adam John Vega <adam.j.vega@hotmail.com>
2014-09-29 20:33:34 -0500

    Delete php_pdo.php

--- php_pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db','adam_vega','NewAtlas88');
-
-// This is the line that gives me errors
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
-
-?>
